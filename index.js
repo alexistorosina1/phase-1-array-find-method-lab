024d4c0964a80c2af1678fd40b3e045eaf8dffd1
Alexis Torosina <alexistorosina1@gmail.com>
2022-09-30 13:56:46 -0400

    commit tree e83b0578fee44766428ad1db064e6e3beca38bc1

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W" },
+    { year: "2016", result: "N/A" },
+    { year: "2017", result: "L" },
+]
+function superbowlWin(record) {
+    let dub = record.find((arr) => arr.result === "W")
+    if(dub){
+        return dub.year
+    }
+}
